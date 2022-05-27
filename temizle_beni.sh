64e58f15b4274d5591db8a51b44f620dbe487e9d
Yunus emre Aktas <yaktas@k2m17s04.42kocaeli.com.tr>
2022-05-27 17:55:01 +0300

    0.3

--- temizle_beni.sh
+++ temizle_beni.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 # Colors
-blue=$'\033[1;34m'
-cyan=$'\033[1;96m'
+purple=$'\033[1;34m'
 reset=$'\033[0;39m'
-
 red=$'\033[0;31m'
 green=$'\033[0;32m'
 
-
+# Commands
 clear;
 echo "--------------------------------------------------------------------------------"
 initial_df=$(df -h . | grep --color=always -E "Size|Used|Avail|Capacity|[0-9]*\.*[0-9]*Mi|[0-9]*\.*[0-9]*Gi|[0-9]+\.*[0-9]+% |$")
-echo -e "${blue}Geçerli alan:\n${reset}${initial_df}${reset}"
-echo -e "${blue}\nAna klasör:${reset}"
-du -hd1 . 2>/dev/null | sort -h | grep --color=always "[0-9]*\.*[0-9]*M\t\|[0-9]*\.*[0-9]*G\t\|$"
+echo -e "${purple}Geçerli alan:\n${reset}${initial_df}${reset}"
 echo ""
 echo "--------------------------------------------------------------------------------"
 cd;
-read -n1 -p "${blue}Bilgisayarınızda yer açmak istermisiniz ?[${green}y${cyan}/${red}N${blue}]${reset} " input
+read -n1 -p "${purple}Bilgisayarınızda yer açmak istermisiniz ?[${green}y${purple}/${red}N${purple}]${reset} " input
 echo ""
 
 if [ -n "$input" ] && [ "$input" = "y" ]; then
@@ -41,22 +37,21 @@ if [ -n "$input" ] && [ "$input" = "y" ]; then
     else
     echo "Temizlik yapılmadı.";
     fi
-read -n1 -p "${blue}İndirilenler klasörünü de temizlemek ister misiniz ?[${green}y${cyan}/${red}N${blue}]${reset} " input
+read -n1 -p "${purple}İndirilenler klasörünü de temizlemek ister misiniz ?[${green}y${purple}/${red}N${purple}]${reset} " input
 echo ""
 if [ -n "$input" ] && [ "$input" = "y" ]; then
         echo "İndirilenler temizleniyor......";
 		rm -rf ./Downloads/*;
     echo "--------------------------------------------------------------------------------"
-    echo -e "${blue}\nTemizlikten önceki alan:\n${reset}${initial_df}${blue}\n\nTemizlikten sonraki alan:${reset}"
+    echo -e "${purple}\nTemizlikten önceki alan:\n${reset}${initial_df}${purple}\n\nTemizlikten sonraki alan:${reset}"
         df -h . | grep --color=always -E "Size|Used|Avail|Capacity|[0-9]*\.*[0-9]*Mi|[0-9]*\.*[0-9]*Gi|[0-9]+\.*[0-9]+% |$"
     echo "--------------------------------------------------------------------------------"
     else
     echo "İndirilenler temizlenmedi.";
     echo "--------------------------------------------------------------------------------"
-    echo -e "${blue}\nTemizlikten önceki alan:\n${reset}${initial_df}${blue}\n\nTemizlikten sonraki alan:${reset}"
+    echo -e "${purple}\nTemizlikten önceki alan:\n${reset}${initial_df}${purple}\n\nTemizlikten sonraki alan:${reset}"
         df -h . | grep --color=always -E "Size|Used|Avail|Capacity|[0-9]*\.*[0-9]*Mi|[0-9]*\.*[0-9]*Gi|[0-9]+\.*[0-9]+% |$"
     echo "--------------------------------------------------------------------------------"
     
 	fi
-
-#
+# powered by erdem149, yeaktas
