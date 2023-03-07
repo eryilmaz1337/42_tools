d16645240b58771dc54281149fcabe62fd632533
Erdem Yilmaz <eryilmaz@k2m06s06.42kocaeli.com.tr>
2023-03-07 13:19:48 +0300

    yeni düzenlemeler yapıldı artık daha fazla yer açıyor :)

--- temizle_beni.sh
+++ temizle_beni.sh
@@ -6,7 +6,6 @@ red=$'\033[0;31m'
 green=$'\033[0;32m'
 
 # Commands
-git pull;
 clear;
 echo "--------------------------------------------------------------------------------"
 initial_df=$(df -h . | grep --color=always -E "Size|Used|Avail|Capacity|[0-9]*\.*[0-9]*Mi|[0-9]*\.*[0-9]*Gi|[0-9]+\.*[0-9]+% |$")
@@ -19,6 +18,7 @@ echo ""
 
 if [ -n "$input" ] && [ "$input" = "y" ]; then
     echo "Temizlik başlıyor lütfen bekleyin...";
+    echo "*************************************************************************************"
     echo "./.Trash/* Temizleniyor lütfen bekleyin..."
     rm -rf ./.Trash/*;
     echo "./.cache/* Temizleniyor lütfen bekleyin..."
@@ -35,6 +35,38 @@ if [ -n "$input" ] && [ "$input" = "y" ]; then
     rm -rf ./Library/Application Support/Code/CachedData/*;
     echo "./Library/Developer/CoreSimulator/* Temizleniyor lütfen bekleyin..."
     rm -rf ./Library/Developer/CoreSimulator/*;
+    echo "*************************************************************************************"
+    echo "-------------------------------------------------------------------------------------"
+    echo "Diğer dosyalar temizleniyor (yeni eklenen silinecek dosyalar) lütfen bekleyin..."
+    echo "-------------------------------------------------------------------------------------"
+    /bin/rm -rf "$HOME"/Library/*.42* &>/dev/null
+    /bin/rm -rf "$HOME"/*.42* &>/dev/null
+    /bin/rm -rf "$HOME"/.zcompdump* &>/dev/null
+    /bin/rm -rf "$HOME"/.cocoapods.42_cache_bak* &>/dev/null
+    /bin/chmod -R 777 "$HOME"/Library/Caches/Homebrew &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Caches/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Caches/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Slack/Cache/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/discord/Cache/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/discord/Code\ Cache/js* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/discord/Crashpad/completed/*  &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Code/Cache/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Code/CachedData/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Code/Crashpad/completed/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Code/User/workspaceStorage/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/Service\ Worker/CacheStorage/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/Application\ Cache/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/Application\ Cache/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Crashpad/completed/* &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Chromium/Default/File\ System &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Chromium/Profile\ [0-9]/File\ System &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/File\ System &>/dev/null
+    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/File\ System &>/dev/null
+    /bin/rm -rf "$HOME"/Desktop/Piscine\ Rules\ *.mp4
+    /bin/rm -rf "$HOME"/Desktop/PLAY_ME.webloc
+    find "$HOME"/Desktop -name .DS_Store -depth -exec /bin/rm {} \; &>/dev/null
     echo "${green}Temizlik yapıldı."
     else
     echo "Temizlik yapılmadı.";
@@ -55,6 +87,6 @@ if [ -n "$input" ] && [ "$input" = "y" ]; then
     echo -e "${purple}\nTemizlikten önceki alan:\n${reset}${initial_df}${purple}\n\nTemizlikten sonraki alan:${reset}"
         df -h . | grep --color=always -E "Size|Used|Avail|Capacity|[0-9]*\.*[0-9]*Mi|[0-9]*\.*[0-9]*Gi|[0-9]+\.*[0-9]+% |$"
     echo "--------------------------------------------------------------------------------"
-    
+
 	fi
 # powered by erdem149, yeaktas
